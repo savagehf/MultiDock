e012fd46c0d7021b70731fdee948c0a510b8835d
savagehf <1213218384@qq.com>
2015-04-15 18:13:11 +0800

    add single project

---
+++ TestSingle/TestSingleView.h
@@ -0,0 +1,62 @@
+// This MFC Samples source code demonstrates using MFC Microsoft Office Fluent User Interface 
+// (the "Fluent UI") and is provided only as referential material to supplement the 
+// Microsoft Foundation Classes Reference and related electronic documentation 
+// included with the MFC C++ library software.  
+// License terms to copy, use or distribute the Fluent UI are available separately.  
+// To learn more about our Fluent UI licensing program, please visit 
+// http://msdn.microsoft.com/officeui.
+//
+// Copyright (C) Microsoft Corporation
+// All rights reserved.
+
+// TestSingleView.h : interface of the CTestSingleView class
+//
+
+#pragma once
+
+
+class CTestSingleView : public CView
+{
+protected: // create from serialization only
+	CTestSingleView();
+	DECLARE_DYNCREATE(CTestSingleView)
+
+// Attributes
+public:
+	CTestSingleDoc* GetDocument() const;
+
+// Operations
+public:
+
+// Overrides
+public:
+	virtual void OnDraw(CDC* pDC);  // overridden to draw this view
+	virtual BOOL PreCreateWindow(CREATESTRUCT& cs);
+protected:
+	virtual BOOL OnPreparePrinting(CPrintInfo* pInfo);
+	virtual void OnBeginPrinting(CDC* pDC, CPrintInfo* pInfo);
+	virtual void OnEndPrinting(CDC* pDC, CPrintInfo* pInfo);
+
+// Implementation
+public:
+	virtual ~CTestSingleView();
+#ifdef _DEBUG
+	virtual void AssertValid() const;
+	virtual void Dump(CDumpContext& dc) const;
+#endif
+
+protected:
+
+// Generated message map functions
+protected:
+	afx_msg void OnFilePrintPreview();
+	afx_msg void OnRButtonUp(UINT nFlags, CPoint point);
+	afx_msg void OnContextMenu(CWnd* pWnd, CPoint point);
+	DECLARE_MESSAGE_MAP()
+};
+
+#ifndef _DEBUG  // debug version in TestSingleView.cpp
+inline CTestSingleDoc* CTestSingleView::GetDocument() const
+   { return reinterpret_cast<CTestSingleDoc*>(m_pDocument); }
+#endif
+
